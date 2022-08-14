ad2d8ba082273c8a5f8ccf6ecaba5e8b037e2175
Ekaterina Anikina <reityan97@mail.ru>
2022-08-14 11:52:33 +0300

    Решение задачи 43

---
+++ ex_001/Program.cs
@@ -0,0 +1,16 @@
+﻿int count = 0;
+Console.WriteLine("Сколько чисел требуется ввести?");
+int m = Convert.ToInt32(Console.ReadLine());
+
+for (int i=0; i<m; i++)
+{
+    Console.Write($"Введите {i+1} число:");
+    int num = Convert.ToInt32(Console.ReadLine());
+    if (num>0) count++;
+}
+
+Console.WriteLine("Было введено:" + count);
+
+
+
+
